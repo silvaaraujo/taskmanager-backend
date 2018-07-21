dca84933117fdd4718a90e205a41e4498bf6dcf1
silvaaraujo <silvaaraujo.dev@gmail.com>
2018-07-21 09:51:13 -0400

    criacao do projeto pelo Spring Tools Suite

---
+++ src/main/java/br/com/silvaaraujo/TaskmanagerBackendApplication.java
@@ -0,0 +1,12 @@
+package br.com.silvaaraujo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskmanagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskmanagerBackendApplication.class, args);
+	}
+}
