cfe13534f407f967fa6ebce13b19241b0e1da6a3
silvaaraujo <silvaaraujo.dev@gmail.com>
2018-07-21 10:29:47 -0400

    criacao do recurso de listagem de tasks e definicao da conexao de BD com mySql

--- src/main/java/br/com/silvaaraujo/resources/TaskResource.java
+++ src/main/java/br/com/silvaaraujo/resources/TaskResource.java
@@ -1,9 +1,14 @@
 package br.com.silvaaraujo.resources;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.silvaaraujo.model.Task;
 import br.com.silvaaraujo.repository.TaskRepository;
 
 
@@ -13,5 +18,11 @@ public class TaskResource {
 
 	@Autowired
 	private TaskRepository taskRepository;
+	
+	@ResponseBody
+	@RequestMapping(method = RequestMethod.GET)
+	public List<Task> listarTodos() {
+		return this.taskRepository.findAll();
+	}
 	
 }
